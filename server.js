d2ac538fd28c82f767efd26b5a2e8b69181c7efc
Garrett Griffey <garrettwgriffey@gmail.com>
2020-04-17 21:01:45 -0500

    connecting server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require("express");
-var path = require("path");
 
 var app = express();
+
 var PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./routes/htmlRoute")(app);
-// require("./routes/apiRoute")(app);
+require("./routes/htmlRoutes.js")(app);
+// require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
