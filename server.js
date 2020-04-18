34b916201508684bba6b58edff37918f25cf964f
Garrett Griffey <garrettwgriffey@gmail.com>
2020-04-17 19:49:38 -0500

    created package-lock.json

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/htmlRoute")(app);
-require("./routes/apiRoute")(app);
+// require("./routes/htmlRoute")(app);
+// require("./routes/apiRoute")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
