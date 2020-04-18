11aef0b98da4551f5a49539c2bbd001fbb76ee9e
Garrett Griffey <garrettwgriffey@gmail.com>
2020-04-18 10:01:09 -0500

    server working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
@@ -7,8 +8,8 @@ var PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/htmlRoutes.js")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoute.js")(app);
+require("./routes/apiRoute.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
