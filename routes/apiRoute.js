a3009529ab2d6208b7d28de280284439da69a1f2
Garrett Griffey <garrettwgriffey@gmail.com>
2020-04-18 16:12:13 -0500

    started apiRoute folder

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,11 +1,10 @@
 var path = require("path");
+var fs = require("fs")
 
 module.exports = function (app) {
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+    app.get
 
-  app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
+    app.post
+    
+    app.delete
 };
